52362e62213841fb418880908111b0ed14e0ab30
loger <logerlink@outlook.com>
2025-08-07 10:15:31 +0800

    fix：补全Module

---
+++ src/modules/user/MyProject.User.Application.Contracts/UserModuleApplicationContractsModule.cs
@@ -0,0 +1,17 @@
+﻿using MyProject.User.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Modularity;
+
+namespace MyProject.User.Application.Contracts
+{
+    [DependsOn(
+        typeof(UserModuleSharedModule)
+    )]
+    public class UserModuleApplicationContractsModule: AbpModule
+    {
+    }
+}
---
+++ src/modules/user/MyProject.User.Shared/UserModuleSharedModule.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Modularity;
+
+namespace MyProject.User.Shared
+{
+    public class UserModuleSharedModule: AbpModule
+    {
+        public override void PreConfigureServices(ServiceConfigurationContext context)
+        {
+            base.PreConfigureServices(context);
+        }
+    }
+}
